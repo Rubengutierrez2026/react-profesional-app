c5cfbd2ac755a4f227a2da0b3feb3a54868b4f7b
rubengutierrez2026 <g.rubendario42@gmail.com>
2026-09-22 21:21:37 -0300

    correccion primer commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+
+import React from 'react'
+
+ function App() {
+  return (
+    <div>
+      <h1>Base de App inicializada</h1>
+    </div>
+  )
+}
+
+export default App
+
